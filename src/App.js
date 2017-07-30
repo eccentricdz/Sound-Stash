29979f854c15d66349960cf383a79963e3616d9b
Rahul Agarwal <rahulaga@amazon.com>
2017-07-30 15:33:54 -0700

    ..getting there

--- src/App.js
+++ src/App.js
@@ -2,7 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBox from './SearchBox'
 
+const SS_BASE_URL = "https://sound-stash.herokuapp.com/"
+const SS_BASE_SEARCH_URL = SS_BASE_URL + "search/"
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  getSearchResults(query) {
+    if (!query)
+      return false;
+
+    const httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+      console.log('Giving up: Cannot create an XMLHTTP instance');
+      return false;
+    }
+
+    const queryUrl = SS_BASE_SEARCH_URL + query
+    httpRequest.open('GET', queryUrl);
+    httpRequest.onreadystatechange = function() {
+      try {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          if (httpRequest.status === 200) {
+            console.log(JSON.parse(httpRequest.responseText));
+          } else {
+            console.log('There was a problem with the request : '+httpRequest.status);
+          }
+        }
+      }
+      catch( e ) {
+        console.log('Caught Exception: ' + e.description);
+      }
+    }
+
+    httpRequest.send();
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +48,7 @@ class App extends Component {
           <h2 id="sound">SOUND</h2><h2 id="stash">STASH</h2>
         </div>
         <div className="App-container">
-          <SearchBox />
+          <SearchBox onChange={this.getSearchResults}/>
         </div>
       </div>
     );
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './SearchBox.css';
 
 class SearchBox extends Component {
+	constructor(props) {
+		super(props)
+		this.handleChange.bind(this)
+	}
+	handleChange(event) {
+		const query = event.target.value
+		this.props.onChange(query)
+	}
 	render() {
 		return (
 			<div className="search-box-container">
-				<input type="text" id="search-box" name="search-box" />
-				<i className="fa fa-search" aria-hidden="true"></i>
+				<form id="search-box-form">
+					<input type="text" id="search-box" name="search-box" onChange={this.handleChange.bind(this)}/>
+					<i className="fa fa-search" aria-hidden="true"></i>
+				</form>
 			</div>
 		)
 	}
