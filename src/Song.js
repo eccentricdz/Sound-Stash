2d62d311d70afcdfe68e21b4f7044cb8439a35b5
Rahul Agarwal <rahulaga@amazon.com>
2017-07-30 22:13:40 -0700

    added dload link

--- src/Song.js
+++ src/Song.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './Song.css'
 
+const SS_BASE_URL = "https://sound-stash.herokuapp.com/"
+
 class Song extends Component {
 
 	prepareInfoObject() {
@@ -8,7 +10,10 @@ class Song extends Component {
 		let videoInfo = {
 			title : rawVideoInfo.snippet.title,
 			channelName : rawVideoInfo.snippet.channelTitle,
-			thumbnailImageUrl : rawVideoInfo.snippet.thumbnails.medium.url
+			thumbnailImageUrl : rawVideoInfo.snippet.thumbnails.medium.url,
+			audioDownloadLink: SS_BASE_URL + 'download/audio/' + rawVideoInfo.id.videoId,
+			videoDownloadLink: SS_BASE_URL + 'download/video/' + rawVideoInfo.id.videoId,
+
 		}
 
 		return videoInfo
@@ -20,14 +25,20 @@ class Song extends Component {
 			backgroundImage: 'url(' + videoInfo.thumbnailImageUrl + ')'
 		}
 		return (
-			<div className='song-container'>
-				<div className='song-thumbnail' style={thumbnailStyle}></div>
-				<div className='song-info-container'>
-					<p className="song-title">{videoInfo.title}</p>
-					<p className="song-channel-name">{videoInfo.channelName}</p>
+			<div className="song-wrapper">
+				<div className='song-container'>
+					<div className='song-thumbnail' style={thumbnailStyle}></div>
+					<div className='song-info-container'>
+						<p className="song-title">{videoInfo.title}</p>
+						<p className="song-channel-name">{videoInfo.channelName}</p>
+					</div>
+					<div className='download-icon-container'>
+						<i className="fa fa-cloud-download" aria-hidden="true"></i>
+					</div>
 				</div>
-				<div className='download-icon-container'>
-					<i className="fa fa-cloud-download" aria-hidden="true"></i>
+				<div className="song-dload-links">
+					<a className="audio-dload-link" href={videoInfo.audioDownloadLink}>audio</a>
+					<a className="video-dload-link" href={videoInfo.videoDownloadLink}>video</a>
 				</div>
 			</div>
 		)
