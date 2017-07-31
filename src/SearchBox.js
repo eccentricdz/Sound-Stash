e36fedbe181d0db77e8b82249dd334aeef7a2f0b
Rahul Agarwal <rahulaga@amazon.com>
2017-07-30 21:16:31 -0700

    ..getting there

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,16 +4,24 @@ import './SearchBox.css';
 class SearchBox extends Component {
 	constructor(props) {
 		super(props)
+
+		this.state = {
+			value: ''
+		}
 	}
 	handleChange(event) {
 		const query = event.target.value
+		this.setState({
+			value: query
+		})
 		this.props.onChange(query)
 	}
 	render() {
+		const className = this.state.value === '' ? '' : 'has-value'
 		return (
 			<div className="search-box-container">
 				<form id="search-box-form">
-					<input type="text" id="search-box" name="search-box" onChange={this.handleChange.bind(this)}/>
+					<input type="text" id="search-box" className={className} name="search-box" onChange={this.handleChange.bind(this)}/>
 					<i className="fa fa-search" aria-hidden="true"></i>
 				</form>
 			</div>
