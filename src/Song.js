6a119b6399339d85a02488f176cf1fff99a5eaab
Rahul Agarwal <rahulaga@amazon.com>
2017-07-30 20:30:21 -0700

    Adding Song component

--- src/Song.js
+++ src/Song.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import './Song.css'
 
 class Song extends Component {
+
+	prepareInfoObject() {
+		const rawVideoInfo = this.props.videoInfo
+		let videoInfo = {
+			title : rawVideoInfo.snippet.title,
+			channelName : rawVideoInfo.snippet.channelTitle,
+			thumbnailImageUrl : rawVideoInfo.snippet.thumbnails.medium.url
+		}
+
+		return videoInfo
+	}
+
 	render() {
-		const videoInfo = this.props.videoInfo
+		const videoInfo = this.prepareInfoObject()
+		const thumbnailStyle = {
+			backgroundImage: 'url(' + videoInfo.thumbnailImageUrl + ')'
+		}
 		return (
-			<p>{videoInfo.snippet.title}</p>
+			<div className='song-container'>
+				<div className='song-thumbnail' style={thumbnailStyle}></div>
+				<div className='song-info-container'>
+					<p className="song-title">{videoInfo.title}</p>
+					<p className="song-channel-name">{videoInfo.channelName}</p>
+				</div>
+				<div className='download-icon-container'>
+					<i className="fa fa-cloud-download" aria-hidden="true"></i>
+				</div>
+			</div>
 		)
 	}
 }
